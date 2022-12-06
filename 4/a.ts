089ba7b41878c22e475f077bf11367be4832ee18
Nathan Selig <nathanselig@gmail.com>
2022-12-06 14:30:22 -0500

    forget to switch to b.ts

--- 4/a.ts
+++ 4/a.ts
@@ -1,43 +1,21 @@
-const input = await Deno.readTextFile('input.txt');
 
-const lines = input.split('\n');
+const input = await Deno.readTextFile("input.txt");
+// const input = await Deno.readTextFile("./test.txt");
 
-/*
-18-20,19-21
-9-86,9-87
-7-8,8-18
-82-98,98-99
-17-17,17-77
-13-21,20-79
-46-52,45-46
-34-54,37-53
-4-91,3-5
-9-80,5-83
-66-83,67-83
-*/
+const pairs = input.split("\n").map((line) => line.split(","));
 
-//In how many assignment pairs does one range fully contain the other?
-const evleExpand = (evle) => {
-    const startFinish = evle.split('-')
-    let zones = []
-    for (let i = startFinish[0] - 1; i <= startFinish[1]; i++) {
-        zones.push(i)
-    }
-    console.log(zones)
-    return zones
-}
+console.log(pairs);
 
-let sum = 0
-for (const line of lines) {
-    const evles = line.split(',')
-    const evle1 = evleExpand(evles[0])
-    const evle2 = evleExpand(evles[1])
+let containedranges = 0;
 
-    //check if all elements of one are in the other 
-    if (evle1.find((zone) => evle2.includes(zone))) {
-        sum++;
-    }
+for (const pair of pairs) {
+    const rangeA = pair[0].split("-").map((x) => parseInt(x));
+    const rangeB = pair[1].split("-").map((x) => parseInt(x));
+
+    const hasOverlap = rangeA[0] <= rangeB[1] && rangeB[0] <= rangeA[1]
+
+    if (hasOverlap) containedranges++;
 
 }
 
-console.log(sum)
+console.log(containedranges);
